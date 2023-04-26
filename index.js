baf287805abe5b6324378995a22d9692fba78b34
jimmyolo <victor0801x@gmail.com>
2023-04-27 00:48:07 +0800

    wip: add status=%s

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ module.exports = {
   addon,
   ready: (pid = process.pid) => addon.ready(pid),
   stopping: (pid = process.pid) => addon.stopping(pid),
+  status: (msg = '') => addon.status(`STATUS=${msg}\n`),
+  watchdog: () => addon.watchdog(),
+  watchdogInterval: () => addon.watchdogInterval(),
 };
--- sdnotify.c
+++ sdnotify.c
@@ -1,14 +1,19 @@
 #include <node_api.h>
 #include <systemd/sd-daemon.h>
-#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define READY "READY=1"
 #define STOPPING "STOPPING=1"
+#define WATCHDOG "WATCHDOG=1"
 
-napi_value ready(napi_env env, napi_callback_info info) {
+napi_value ready(napi_env env, napi_callback_info info)
+{
   size_t argc = 1;
-  napi_value argv[1];
+  napi_value argv[argc];
   napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
 
   int32_t pid;
@@ -22,9 +27,10 @@ napi_value ready(napi_env env, napi_callback_info info) {
   return NULL;
 }
 
-napi_value stopping(napi_env env, napi_callback_info info) {
+napi_value stopping(napi_env env, napi_callback_info info)
+{
   size_t argc = 1;
-  napi_value argv[1];
+  napi_value argv[argc];
   napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
 
   int32_t pid;
@@ -38,7 +44,14 @@ napi_value stopping(napi_env env, napi_callback_info info) {
   return NULL;
 }
 
-napi_value is_watchdog_enabled(napi_env env, napi_callback_info info) {
+napi_value watchdog(napi_env env, napi_callback_info info)
+{
+  sd_notify(0, WATCHDOG);
+  return NULL;
+}
+
+napi_value watchdog_interval(napi_env env, napi_callback_info info)
+{
   uint64_t usec = 0;
   int enabled = sd_watchdog_enabled(0, &usec);
   if (enabled > 0) {
@@ -58,7 +71,41 @@ napi_value is_watchdog_enabled(napi_env env, napi_callback_info info) {
   }
 }
 
-napi_value init_all(napi_env env, napi_value exports) {
+char *read_js_string(napi_env env, napi_value js_value)
+{
+  size_t len;
+  napi_get_value_string_utf8(env, js_value, NULL, 0, &len);
+
+  napi_status status;
+
+  char *value = (char *)malloc(len + 1);
+  status = napi_get_value_string_utf8(env, js_value, value, len + 1, &len);
+  if (status != napi_ok) {
+    free(value);
+    napi_throw_error(env, NULL, "Unable to convert js string to char *");
+    return NULL;
+  }
+  // printf("%s", value);
+
+  return value;
+}
+
+napi_value status(napi_env env, napi_callback_info info)
+{
+  size_t argc = 1;
+  napi_value argv[argc];
+  napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+
+  char *js_string = read_js_string(env, argv[0]);
+
+  sd_notify(0, js_string);
+
+  free(js_string);
+  return NULL;
+}
+
+napi_value Init(napi_env env, napi_value exports)
+{
   napi_value fn;
 
   napi_create_function(env, NULL, 0, ready, NULL, &fn);
@@ -67,10 +114,16 @@ napi_value init_all(napi_env env, napi_value exports) {
   napi_create_function(env, NULL, 0, stopping, NULL, &fn);
   napi_set_named_property(env, exports, "stopping", fn);
 
-  napi_create_function(env, NULL, 0, is_watchdog_enabled, NULL, &fn);
-  napi_set_named_property(env, exports, "isWatchdogEnabled", fn);
+  napi_create_function(env, NULL, 0, watchdog, NULL, &fn);
+  napi_set_named_property(env, exports, "watchdog", fn);
+
+  napi_create_function(env, NULL, 0, watchdog_interval, NULL, &fn);
+  napi_set_named_property(env, exports, "watchdogInterval", fn);
+
+  napi_create_function(env, NULL, 0, status, NULL, &fn);
+  napi_set_named_property(env, exports, "status", fn);
 
   return exports;
 }
 
-NAPI_MODULE(NODE_GYP_MODULE_NAME, init_all)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
