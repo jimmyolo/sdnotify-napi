cf15f4bf5f053095ca229f1cb0dc5133039e14c3
jimmyolo <victor0801x@gmail.com>
2023-04-27 01:09:41 +0800

    wip: add start/stop watchdog mode

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const addon = require("node-gyp-build")(__dirname);
 
+let watchdogTimeout = null;
+
 module.exports = {
   addon,
   ready: (pid = process.pid) => addon.ready(pid),
@@ -7,4 +9,13 @@ module.exports = {
   status: (msg = '') => addon.status(`STATUS=${msg}\n`),
   watchdog: () => addon.watchdog(),
   watchdogInterval: () => addon.watchdogInterval(),
+  startWatchdogMode: (interval) => {
+    interval = +interval ?? 3_000
+    watchdogTimeout && clearInterval(watchdogTimeout)
+    watchdogTimeout = setInterval(addon.watchdog, interval)
+  },
+  stopWatchdogMode: () => {
+    watchdogTimeout && clearInterval(watchdogTimeout)
+    watchdogTimeout = null
+  },
 };
